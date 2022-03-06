e08d5411c59b5c761e988b105aa55dc41cb71bbb
MichalSuper <superfin@g.jct.ac.il>
2022-03-06 23:50:44 +0200

    first targil

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -7,16 +7,31 @@ public class Plane implements Geometry{
     private Point q0;
     private Vector normal;
 
+    /**
+     * constructor
+     * @param p1 first point
+     * @param p2 second point
+     * @param p3 third point
+     */
     public Plane(Point p1, Point p2, Point p3){
         q0= p1;
         normal= null;
     }
 
+    /**
+     * constructor
+     * @param p a point
+     * @param v a vector
+     */
     public Plane(Point p, Vector v){
         q0= p;
         normal= v.normalize();
     }
 
+    /**
+     * to string
+     * @return description of the plane
+     */
     @Override
     public String toString() {
         return "Plane{" +
@@ -33,6 +48,11 @@ public class Plane implements Geometry{
         return this.normal;
     }
 
+    /**
+     * return the normal of the plane
+     * @param p a point
+     * @return normal
+     */
     public Vector getNormal(Point p){
         return this.normal;
     }
--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -3,6 +3,13 @@ package geometries;
 import primitives.Point;
 
 public class Triangle extends Polygon{
+
+    /**
+     * constructor
+     * @param p1 first point
+     * @param p2 second point
+     * @param p3 third point
+     */
     public Triangle(Point p1, Point p2, Point p3) {
         super(p1, p2, p3);
     }
