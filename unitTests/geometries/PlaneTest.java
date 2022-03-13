0376d1265456dbcf398e487674a610b3eda0c8ba
MichalSuper <superfin@g.jct.ac.il>
2022-03-14 00:51:40 +0200

    tube

--- unitTests/geometries/PlaneTest.java
+++ unitTests/geometries/PlaneTest.java
@@ -20,7 +20,7 @@ class PlaneTest {
         // ============ Equivalence Partitions Tests ==============
         Plane pl= new Plane(new Point(0,0,1),new Point(1,0,0),new Point(0,1,0));
         double sqrt3= Math.sqrt(1d/3);
-        assertEquals(new Vector(sqrt3,sqrt3,sqrt3),pl.getNormal(new Point(0,0,1)),"Wrong normal to sphere");
+        assertEquals(new Vector(sqrt3,sqrt3,sqrt3),pl.getNormal(new Point(0,0,1)),"Wrong normal to plane");
     }
 
 }
--- unitTests/geometries/TubeTest.java
+++ unitTests/geometries/TubeTest.java
@@ -2,6 +2,8 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +18,14 @@ class TubeTest {
      */
     @Test
     void testGetNormal() {
-
+        // ============ Equivalence Partitions Tests ==============
+        Tube tube = new Tube(new Ray(new Point(0,0,1),new Vector(0,-1,0)),1.0);
+        Vector normal = tube.getNormal(new Point(0,0.5,2)).normalize();
+        double dotProduct = normal.dotProduct(tube.getAxisRay().getDir());
+        assertEquals(0d,dotProduct,"normal is not orthogonal to the tube");
+        boolean firstNormal = new Vector(0,0,1).equals(normal);
+        boolean secondNormal = new Vector(0,0,-1).equals(normal);
+        assertTrue(firstNormal||secondNormal,"wrong normal to tube");
+        assertEquals(new Vector(0,0,1),normal,"wrong normal to tube");
     }
 }
